92b90a8fa3d5cfc2fe76ca9c36dd624dbe924617
malyadr <malyadri.mekapati@zemosolabs.com>
2022-03-29 13:48:23 +0530

    o

--- web-ui/src/utils/resources/resources.js
+++ web-ui/src/utils/resources/resources.js
@@ -1,4 +1,4 @@
-const { BASE_URL } = window.env || {BASE_URL : 'http://a090bdf559bcc4f4798bc3d09b9e2477-854998059.ap-south-1.elb.amazonaws.com:8080'};
+const { BASE_URL } = window.env || {BASE_URL : 'http://localhost:8080'};
 
 export const apis = {
   GET_SKILLS: `${BASE_URL}/api/skills`,

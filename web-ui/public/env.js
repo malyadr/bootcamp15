21f50d099a4a8148a0231ac96f44b8a4def57ba9
malyadr <malyadri.mekapati@zemosolabs.com>
2022-03-29 12:39:27 +0530

    o

--- web-ui/public/env.js
+++ web-ui/public/env.js
@@ -1,3 +1,3 @@
 window.env = Object.freeze({
-    BASE_URL: 'a43d5a90f9a094b7d966ead0de7fb2cf-161477199.ap-south-1.elb.amazonaws.com:8080',
+    BASE_URL: 'http://a090bdf559bcc4f4798bc3d09b9e2477-854998059.ap-south-1.elb.amazonaws.com:8080',
 });
